b4e131237e9d25c70581adbcc65fb5f298675c1a
zhik <zhkseniya88@gmail.com>
2019-09-23 16:34:51 +0600

    Добавляет просмотр фото в полноэкранном режиме

--- js/main.js
+++ js/main.js
@@ -18,6 +18,27 @@ var COMMENTS = {
 };
 
 var similarListElement = document.querySelector('.pictures');
+var bigPicture = document.querySelector('.big-picture');
+var commentCount = document.querySelector('.social__comment-count');
+var commentsLoader = document.querySelector('.comments-loader');
+
+var showElement = function (element, className) {
+  element.classList.remove(className);
+};
+
+var hiddenElement = function (element, className) {
+  element.classList.add(className);
+};
+
+var removeChildren = function (element) {
+  var children = element.children;
+
+  for (var i = children.length - 1; i >= 0; i--) {
+    var child = children[i];
+    child.parentElement.removeChild(child);
+  }
+
+};
 
 var getRandomElement = function (min, max) {
   var element = min + Math.random() * (max + 1 - min);
@@ -44,13 +65,13 @@ var getDescriptionPhoto = function (count) {
   var descriptions = [];
 
   for (var i = 1; i <= count; i++) {
-    var commentCount = getRandomElement(COMMENTS.MIN, COMMENTS.MAX);
+    var commentsCount = getRandomElement(COMMENTS.MIN, COMMENTS.MAX);
 
     var description = {
       url: 'photos/' + i + '.jpg',
       description: 'описание- ' + i,
       likes: getRandomElement(LIKES.MIN, LIKES.MAX),
-      comments: getRandomComments(commentCount)
+      comments: getRandomComments(commentsCount)
     };
 
     descriptions.push(description);
@@ -82,5 +103,48 @@ var renderPhotos = function (array) {
   similarListElement.appendChild(fragment);
 };
 
+var makeElement = function (tagName, className, text) {
+  var element = document.createElement(tagName);
+  element.classList.add(className);
+  if (text) {
+    element.textContent = text;
+  }
+  return element;
+};
+
+var createComment = function (comment) {
+  var listItem = makeElement('li', 'social__comment');
+
+  var avatar = makeElement('img', 'social__picture');
+  avatar.src = comment.avatar;
+  avatar.alt = comment.name;
+  listItem.appendChild(avatar);
+
+  var text = makeElement('p', 'social__text', comment.message);
+  listItem.appendChild(text);
+
+  return listItem;
+};
+
+var createPhotoContent = function (data) {
+  document.querySelector('.big-picture__img img').src = data.url;
+  document.querySelector('.likes-count').textContent = data.likes;
+  document.querySelector('.comments-count').textContent = data.comments.length;
+  var socialComments = document.querySelector('.social__comments');
+  removeChildren(socialComments);
+
+  var comments = data.comments;
+  for (var i = 0; i < comments.length; i++) {
+    var commentsItem = createComment(comments[i]);
+    socialComments.appendChild(commentsItem);
+  }
+
+  document.querySelector('.social__caption').textContent = data.description;
+};
+
 var photos = getDescriptionPhoto(PHOTOS_COUNT);
 renderPhotos(photos);
+showElement(bigPicture, 'hidden');
+createPhotoContent(photos[0]);
+hiddenElement(commentCount, 'visually-hidden');
+hiddenElement(commentsLoader, 'visually-hidden');
